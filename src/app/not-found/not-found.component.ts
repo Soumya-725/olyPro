c415a42e864870f28485792635dfedeaa91d320d
Sourav Das <sourav.das@digitalavenues.com>
2020-02-14 17:24:12 +0530

    not found page done

--- src/app/not-found/not-found.component.ts
+++ src/app/not-found/not-found.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Location } from '@angular/common';
 @Component({
   selector: 'not-found',
   templateUrl: './not-found.component.html',
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotFoundComponent implements OnInit {
 
-  constructor() { }
+  constructor(private location: Location) { }
 
   ngOnInit() {
   }
-
+  backClicked() {
+    this.location.back();
+  }
 }
